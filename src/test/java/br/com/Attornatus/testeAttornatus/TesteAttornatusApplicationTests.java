66496eab1ffbe43421dbc4756cbfdc025b7bca98
Cleverson Farias <cleverson_sairaf@outlook.com>
2023-08-31 16:43:32 -0300

    init commit

---
+++ src/test/java/br/com/Attornatus/testeAttornatus/TesteAttornatusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Attornatus.testeAttornatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteAttornatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
