486181c96c109335e3137cb5e2487a6b4a388fa4
Cleverson Farias <cleverson_sairaf@outlook.com>
2023-09-04 20:54:20 -0300

    implementa controller e fluxo para service

--- src/main/java/br/com/Attornatus/testeAttornatus/pessoa/application/api/PessoaController.java
+++ src/main/java/br/com/Attornatus/testeAttornatus/pessoa/application/api/PessoaController.java
@@ -1,15 +1,20 @@
 package br.com.Attornatus.testeAttornatus.pessoa.application.api;
 
+import br.com.Attornatus.testeAttornatus.pessoa.application.service.PessoaService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Log4j2
+@RequiredArgsConstructor
 public class PessoaController implements PessoaAPI {
+    private final PessoaService pessoaService;
 
     @Override
     public PessoaResponse postPessoa(PessoaRequest pessoaRequest) {
        log.info("[inicia] PessoaController - postPessoa");
+       PessoaResponse pessoaCriado = pessoaService.criaPessoa(pessoaRequest);
        log.info("[finaliza] PessoaController - postPessoa");
        return null;
     }
